f26a5ebf70c3c93b11c2762dcacb028c93a7adcc
Mikhail Masyagin <mikhailmasyagin@fastforward.pro>
2025-04-10 00:05:32 +0000

    FEAT: lecture 05

---
+++ lecture-05/problems/compiler_reordering.c
@@ -0,0 +1,14 @@
+// https://godbolt.org/
+// использовать x86-64 gcc 14.2 -O3 & -O0
+// а ещё можно использовать -masm=att...
+// а ещё и ARM 64 GCC 9.5.0 -O3 & ARM 64 GCC 14.2.0 -O3...
+
+#include <stdatomic.h>
+
+int a = 0;
+int b = 0;
+
+void dummy() {
+    a = b + 2;
+    b = 1;
+}
